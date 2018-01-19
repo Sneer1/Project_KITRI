d69a1660bdddaf2102b4929ef29718790bb109ab
KITRI-PC\KITRI <starchoi91@naver.com>
2018-01-19 12:55:15 +0900

    테스트 입니다.

---
+++ Assets/KTRScript/Panel_GameMenu.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panel_GameMenu : MonoBehaviour
+{
+    Animator _animator;
+
+    public GameObject btLeft;
+    public GameObject btRight;
+    public GameObject btHeart;
+
+    bool bOpened = false;
+
+    // Use this for initialization
+    void Start () {
+        _animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClickMainMenu()
+    {
+        bOpened = !bOpened;
+        if (bOpened)
+        {
+            btLeft.SetActive(true);
+            btRight.SetActive(true);
+            btHeart.SetActive(true);
+        }
+        else
+        {
+            //btLeft.SetActive(false);
+            //btRight.SetActive(false);
+            //btHeart.SetActive(false);
+        }
+
+        _animator.SetBool("isOpen", bOpened);
+    }
+
+    public void OnAnimEndBtClose()
+    {
+        btLeft.SetActive(false);
+        btRight.SetActive(false);
+        btHeart.SetActive(false);
+    }
+
+    public void OnClickBtLeft()
+    {
+
+    }
+
+    public void OnClickBtRight()
+    {
+
+    }
+
+    public void OnClickBtHeart()
+    {
+
+    }
+}
