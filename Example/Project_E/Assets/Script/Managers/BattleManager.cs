2fb2a7218f97a505394a31603cd99389cee7c105
Sneer1 <starchoi91@naver.com>
2018-02-07 12:31:57 +0900

    정식 커밋

---
+++ Example/Project_E/Assets/Script/Managers/BattleManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoSingleton<BattleManager>
+{
+	void Start ()
+    {
+        LoadBattle();
+	}
+
+    public void LoadBattle()
+    {
+        
+        ActorManager.Instance.PlayerLoad();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
