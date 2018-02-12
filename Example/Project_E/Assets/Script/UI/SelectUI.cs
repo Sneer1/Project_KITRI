b1858e324f9b3419e21b6aecc99449536acdd4c4
rlaa54 <32154232+rlaa54@users.noreply.github.com>
2018-02-12 18:24:03 +0900

    민웅
    커밋

--- Example/Project_E/Assets/Script/UI/SelectUI.cs
+++ Example/Project_E/Assets/Script/UI/SelectUI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,113 +9,131 @@ public class SelectUI : MonoBehaviour
     Transform myTransform;
     SelectCharacterData SelectCharacterData = new SelectCharacterData();
 
-    List<GameObject> mylistGameObject = new List<GameObject>();
+    List<GameObject> myCharacterlistGameObject = new List<GameObject>();
 
-    List<string> listSelectCharacter;
+    List<string> StrlistCharacter;
     List<Sprite> listsprite = new List<Sprite>();
     List<GameObject> SelectCharacterlist = new List<GameObject>();
+
+    GameObject ConfirmButton;
 
     Dictionary<ESELECTCHARACTERSTAGE, List<string>> SelectCharacterDic;
 
     ESELECTCHARACTERSTAGE eSELECTCHARACTERSTAGE = ESELECTCHARACTERSTAGE.STAGE_3;
 
+    public List<ECHARACTER> SelectCharacter = new List<ECHARACTER>();
+
     void SelectMusic()
     {
-
 
     }
 
     void SetButton()
     {
-        for (int i = 0; i < listSelectCharacter.Count; ++i)
+        ConfirmButton = GameObject.Find("Confirm").gameObject;
+
+        for (int i = 1; i < 3; ++i)
         {
-            mylistGameObject.Add(Instantiate(Resources.Load<GameObject>("Prefabs/UI/SELECT/Button" + ((ECHARACTER)i).ToString()), myTransform));
+            SelectCharacterlist.Add(GameObject.Find("SelectCharacter" + i).gameObject);
         }
 
-        for (int j = 0; j < mylistGameObject.Count; ++j)
+        for (int i = 0; i < StrlistCharacter.Count; ++i)
         {
-            mylistGameObject[j].GetComponent<Image>().sprite = listsprite[j];
+            myCharacterlistGameObject.Add(GameObject.Find("Button" + ((ECHARACTER)i).ToString()).gameObject);
+            myCharacterlistGameObject[i].GetComponent<Image>().enabled = true;
+            myCharacterlistGameObject[i].GetComponent<Button>().interactable = true;
         }
-
-        switch (mylistGameObject.Count)
-        {
-            case 2:
-                {
-                    mylistGameObject[0].transform.localPosition = new Vector3(-300, -160f, 0f);
-                    mylistGameObject[1].transform.localPosition = new Vector3(300, -160f, 0f);
-                }
-                break;
-
-            case 3:
-                {
-                    mylistGameObject[0].transform.localPosition = new Vector3(-300, -160f, 0f);
-                    mylistGameObject[1].transform.localPosition = new Vector3(0, -160f, 0f);
-                    mylistGameObject[2].transform.localPosition = new Vector3(300, -160f, 0f);
-                }
-                break;
-
-            case 4:
-                {
-                    mylistGameObject[0].transform.localPosition = new Vector3(-450, -160f, 0f);
-                    mylistGameObject[1].transform.localPosition = new Vector3(-150, -160f, 0f);
-                    mylistGameObject[2].transform.localPosition = new Vector3(150, -160f, 0f);
-                    mylistGameObject[3].transform.localPosition = new Vector3(450, -160f, 0f);
-                }
-                break;
-        }
-
-        Instantiate(Resources.Load<GameObject>("Prefabs/UI/SELECT/Confirm"), myTransform);
     }
-    public List<ECHARACTER> CharacterConfirm()
+
+    public void ConfirmClicked()
     {
         List<ECHARACTER> list = new List<ECHARACTER>();
-        list = null;
         string ename = null;
-        for(int i = 0; i < SelectCharacterlist.Count; ++i)
+        ECHARACTER Character_enum;
+        for (int i = 0; i < SelectCharacterlist.Count; ++i)
         {
             ename = SelectCharacterlist[i].GetComponent<Image>().sprite.name;
-            ECHARACTER Character_enum = (ECHARACTER)System.Enum.Parse(typeof(ECHARACTER), ename);
+            Character_enum = (ECHARACTER)System.Enum.Parse(typeof(ECHARACTER), ename);
             list.Add(Character_enum);
         }
-        return list;
+        SelectCharacter = list;
     }
 
     public void CharacterClickedButton(int index)
     {
         for (int i = 0; i < SelectCharacterlist.Count; ++i)
         {
-            if (SelectCharacterlist[i].GetComponent<Image>().sprite == null)
+            if (SelectCharacterlist[i].GetComponent<Image>().sprite != null)
             {
-                SelectCharacterlist[i].GetComponent<Image>().sprite = listsprite[index];
-                return;
+                if (SelectCharacterlist[i].GetComponent<Image>().sprite.name.Equals(listsprite[index].name))
+                    return;
+            }
+        }
+
+        if (SelectCharacterlist[0].GetComponent<Image>().sprite == null)
+        {
+            SelectCharacterlist[0].GetComponent<Image>().sprite = listsprite[index];
+            SelectCharacterlist[0].GetComponent<Image>().enabled = true;
+            SelectCharacterlist[0].GetComponent<Button>().interactable = true;
+        }
+        else if (SelectCharacterlist[1].GetComponent<Image>().sprite == null)
+        {
+            SelectCharacterlist[1].GetComponent<Image>().sprite = listsprite[index];
+            SelectCharacterlist[1].GetComponent<Image>().enabled = true;
+            SelectCharacterlist[1].GetComponent<Button>().interactable = true;
+        }
+
+        if (SelectCharacterlist[0].GetComponent<Image>().sprite != null && SelectCharacterlist[1].GetComponent<Image>().sprite != null)
+        {
+            ConfirmButton.GetComponent<Image>().enabled = true;
+            ConfirmButton.GetComponent<Button>().interactable = true;
+        }
+
+        for (int j = 0; j < SelectCharacterlist.Count; ++j)
+        {
+            if (SelectCharacterlist[j].GetComponent<Image>().sprite != null)
+            {
+                if (SelectCharacterlist[j].GetComponent<Image>().sprite.name.Equals(myCharacterlistGameObject[index].GetComponent<Image>().sprite.name))
+                    myCharacterlistGameObject[index].GetComponent<Image>().color = new Color(0.3f, 0.3f, 0.3f);
             }
         }
     }
 
     public void SelectCharacterClicked(int index)
     {
+        for (int i = 0; i < myCharacterlistGameObject.Count; ++i)
+        {
+            if (SelectCharacterlist[index].GetComponent<Image>().sprite.name.Equals(myCharacterlistGameObject[i].GetComponent<Image>().sprite.name))
+                myCharacterlistGameObject[i].GetComponent<Image>().color = new Color(1f, 1f, 1f);
+        }
+
         SelectCharacterlist[index].GetComponent<Image>().sprite = null;
+        SelectCharacterlist[index].GetComponent<Image>().enabled = false;
+        SelectCharacterlist[index].GetComponent<Button>().interactable = false;
+
+        if (SelectCharacterlist[0].GetComponent<Image>().sprite == null || SelectCharacterlist[1].GetComponent<Image>().sprite == null)
+        {
+            ConfirmButton.GetComponent<Image>().enabled = false;
+            ConfirmButton.GetComponent<Button>().interactable = false;
+        }
     }
-    
+
     void Init()
     {
-        Instantiate(Resources.Load<GameObject>("Prefabs/UI/SELECT/Select_Canvas"), this.transform);
+        //Instantiate(Resources.Load<GameObject>("Prefabs/UI/SELECT/Select_Canvas"), this.transform);
         myTransform = GameObject.Find("Select_Panel").transform;
         SelectCharacterDic = SelectCharacterData.LoadJSONSelectCharacterDic("JSON/STAGE_CHARACTER_DATA");
-        listSelectCharacter = SelectCharacterDic[eSELECTCHARACTERSTAGE];
+        StrlistCharacter = SelectCharacterDic[eSELECTCHARACTERSTAGE];
 
         for (int i = 0; i < (int)ECHARACTER.MAX; ++i)
         {
             listsprite.Add(Resources.Load<Sprite>("Prefabs/UI/Images/" + ((ECHARACTER)i).ToString()));
         }
-        for (int i = 1; i < 3; ++i)
-        {
-            SelectCharacterlist.Add(Instantiate(Resources.Load<GameObject>("Prefabs/UI/SELECT/SelectCharacter" + i), myTransform));
-        }
+
         SetButton();
     }
 
-    private void Awake()
+    private void Start()
     {
         Init();
     }
