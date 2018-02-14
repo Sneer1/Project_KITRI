81b7855a4ba32acb3a41af7160fff546b757f420
SNEER <starchoi91@naver.com>
2018-02-14 18:33:54 +0900

    2월 14일 집에서 커밋

--- Example/Project_E/Assets/Script/Intro_End_Text/StringData.cs
+++ Example/Project_E/Assets/Script/Intro_End_Text/StringData.cs
@@ -5,24 +5,38 @@ using SimpleJSON;
 
 public class StringData : MonoBehaviour
 {
-    string _StrKey = string.Empty;
-    List<string> _StringData = new List<string>();
+    //string _StrKey = string.Empty;
+    //List<string> _StringData = new List<string>();
 
-    public string StrKey { get { return _StrKey; } }
-    public List<string> StringTextData { get { return _StringData; } }
+    //public string StrKey { get { return _StrKey; } }
+    //public List<string> StringTextData { get { return _StringData; } }
 
-    public StringData(string strKey, JSONNode nodeData)
-    {
-        _StrKey = strKey;
-        _Range = nodeData["RANGE"].AsFloat;
+    //public StringData(string strKey, JSONNode nodeData)
+    //{
+    //    TextAsset stringAssetData = Resources.Load(ConstValue.TextDataPath) as TextAsset;
 
-        JSONArray arrSkill = nodeData["SKILL"].AsArray;
-        if (arrSkill != null)
-        {
-            for (int i = 0; i < arrSkill.Count; ++i)
-            {
-                _SkillTemplateList.Add(arrSkill[i]);
-            }
-        }
-    }
+    //    if (stringAssetData == null)
+    //    {
+    //        Debug.LogError("스킬 데이터 로드 실패");
+    //        return;
+    //    }
+    //    JSONNode rootNode = JSON.Parse(stringAssetData.text);
+    //    if (rootNode == null)
+    //        return;
+
+    //    JSONObject stringDataNode = rootNode[ConstValue.SkillDataKey]
+
+
+    //    _StrKey = strKey;
+    //    _Range = nodeData["RANGE"].AsFloat;
+
+    //    JSONArray arrSkill = nodeData["SKILL"].AsArray;
+    //    if (arrSkill != null)
+    //    {
+    //        for (int i = 0; i < arrSkill.Count; ++i)
+    //        {
+    //            _SkillTemplateList.Add(arrSkill[i]);
+    //        }
+    //    }
+    //}
 }
